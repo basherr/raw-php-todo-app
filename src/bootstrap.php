a26f66edc344d0ed8e9099ae8aa34fd47cf7c1e0
Habib <bsr.anwar@outlook.com>
2016-11-19 18:37:24 +0500

    Add filip whoops errror handler

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,1 +1,24 @@
 <?php
+
+require '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+/**
+ * Register error handler
+ */
+$whoops = new Filip\whoops;
+
+$environment = 'development';
+
+if($environment == 'development') {
+	$whoops->pushHandler(new \whoops\Handler\PrettyPageHandler);
+} else {
+	$whoops->pushHandler(function($e) {
+		echo 'Something went wrong!';
+	});
+}
+
+$whoops->register();
+
+throw new \Exception;
